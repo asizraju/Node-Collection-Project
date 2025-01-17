2565465d49878e37ad859f1cda05fccd38914c76
RAJKUMAR M <rajkumarcivil55@gmail.com>
2025-01-17 12:46:12 +0530

    Your message here

---
+++ Route/UserRoute.js
@@ -0,0 +1,19 @@
+const express = require ("express")
+
+const UserController = require ("../Controller/UserController")
+const {verifytoken,adminonly,staffonly} = require ("../Middleware/AuthMiddleware")
+ 
+const router = express.Router()
+
+router.post ("/signup",UserController.createUser)
+router.post("/login",UserController.loginUser)
+router.get("/list",verifytoken,staffonly,UserController.list)
+router.delete("/delete/:id",UserController.delete)
+router.put("/updated/:id",UserController.update)
+
+router.get("/fetchUserlistID",UserController.fetchUserID)
+
+router.get("/fetchUserlistIDS/:id",UserController.fetchUserIDS)
+
+
+module.exports = router;
